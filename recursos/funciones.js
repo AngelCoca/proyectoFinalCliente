0375bf3464cbdb47c6417db7b40a4207e8a03159
Angel <mira19029@iesmarenostrum.com>
2019-02-15 11:53:17 +0100

    Delete cookies

--- recursos/funciones.js
+++ recursos/funciones.js
@@ -623,3 +623,34 @@ function cambiarFecha(fecha){
 
 
 /*---------RESERVAS-----------*/
+
+
+function deleteAllCookies(){
+    document.cookie.split(";").forEach(function(c) { 
+        document.cookie = c.replace(/^ +/, "").replace(/=.*/, "=;expires=" 
+        + new Date().toUTCString() + ";path=/"); });
+
+        var notification = null;
+
+        if (!('Notification' in window)) {
+            // el navegador no soporta la API de notificaciones
+            alert('Su navegador no soporta la API de Notificaciones :(');
+            return;
+        } else if (Notification.permission === "granted") {
+            // Se puede emplear las notificaciones
+
+            notification = new Notification(
+                    "Hola Mundo");
+
+        } else if (Notification.permission !== 'denied') {
+            // se pregunta al usuario para emplear las notificaciones
+            Notification
+                    .requestPermission(function(permission) {
+                if (permission === "granted") {
+                    notification = new Notification(
+                            "Hola Mundo");
+                }
+            });
+        }
+        
+}
